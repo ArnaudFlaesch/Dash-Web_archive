e0843d85617518fdd6d06c57d04342c819ae9bb9
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2020-04-20 22:22:45 -0400

    WIP Test with mock

--- src/tests/widgets/weather/WeatherWidget.test.tsx
+++ src/tests/widgets/weather/WeatherWidget.test.tsx
@@ -4,75 +4,47 @@ import { WeatherWidget } from '../../../widgets/weather/WeatherWidget';
 import * as Adapter from 'enzyme-adapter-react-16';
 import * as Enzyme from 'enzyme';
 
+import axios from 'axios';
+import { render } from 'enzyme';
+
+jest.mock('axios');
+
+
+
 Enzyme.configure({ adapter: new Adapter() });
-export default {
-  get: jest.fn().mockResolvedValue({ data: {} })
-};
+//jest.mock('axios');
 
 describe('Weather widget tests', () => {
+
+  let mock: any;
+  beforeEach(() => {
+    mock = jest.spyOn(axios, 'get');
+  });
+  afterEach(() => {
+    mock.mockRestore();
+  });
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<WeatherWidget />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  /*
   it('Should display the current weather', async () => {
-    const apiWeatherResponse: IWeather = {
-      "coord": {
-        "lon": -73.59,
-        "lat": 45.51
-      },
-      "weather": [
-        {
-          "id": 803,
-          "main": "Clouds",
-          "description": "nuageux",
-          "icon": "04d"
-        }
-      ],
-      "base": "stations",
-      "main": {
-        "temp": -1.55,
-        "feels_like": -8.53,
-        "temp_min": -3,
-        "temp_max": 0,
-        "pressure": 1032,
-        "humidity": 33
-      },
-      "visibility": 48279,
-      "wind": {
-        "speed": 5.1,
-        "deg": 270,
-        "gust": 8.2
-      },
-      "clouds": {
-        "all": 75
-      },
-      "dt": 1584826021,
-      "sys": {
-        "type": 1,
-        "id": 943,
-        "country": "CA",
-        "sunrise": 1584788074,
-        "sunset": 1584832086
-      },
-      "timezone": -14400,
-      "id": 6077243,
-      "name": "Montréal",
-      "cod": 200
-    };
-
-    const mock = new MockAdapter(axios);
-    mock.onGet('/*').reply(200, apiWeatherResponse);
-    const component = await shallow(
-      <WeatherWidget city={"Montréal"} weather_api_key={"weather_api_key"} />,
+    //const mock = new MockAdapter(axios);
+    //mock.onAny('/*').reply(200, apiWeatherResponse);
+    //axios.get = jest.fn() as jest.Mock;
+    //axios.get.mockImplementation(() => Promise.resolve(apiWeatherResponse));
+    render(<WeatherWidget city={"Montréal"} weather_api_key={"d10750704319701c3f9436134add4d7d"} />)
+    const spyDidMount = jest.spyOn(WeatherWidget.prototype, "componentDidMount");
+/*
+    const component = await Enzyme.shallow(
+      <WeatherWidget city={"Montréal"} weather_api_key={"d10750704319701c3f9436134add4d7d"} />,  { disableLifecycleMethods: true }
     );
-    component.update().;
+    */
+expect(axios.get).toHaveBeenCalled();
 
-    expect(component.find('#header').text()).toEqual('La météo aujourd\'hui à Montréal')
+    //expect(component.find('#header').text()).toEqual('La météo aujourd\'hui à Montréal')
   });
-
-  */
 })
 
