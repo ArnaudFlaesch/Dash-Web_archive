167142bb2f8f5a69f98533c7cf1d8a8396c3b777
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2020-05-18 23:12:01 -0400

    FT #17 : Widget store page to add them to the dashboard (#41)

--- src/Dash.tsx
+++ src/Dash.tsx
@@ -1,8 +1,12 @@
 import axios from "axios";
 import 'font-awesome/fonts/fontawesome-webfont.svg';
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './Dash.scss';
 import { WidgetTypes } from './enums/WidgetsEnum';
+import Navbar from './navbar/Navbar';
+import Store from './pages/store/Store';
+import { addWidget } from './services/WidgetService';
 import logger from './utils/LogUtils';
 import CalendarWidget from './widgets/calendar/CalendarWidget';
 import { RSSWidget } from "./widgets/rss/RSSWidget";
@@ -18,13 +22,35 @@ interface IWidgetConfig {
 	data: any;
 }
 
+export interface IMenu {
+	link: string;
+	icon: string;
+}
+
 export default class Dashboard extends React.Component<any, IState> {
+
+	private navItems: IMenu[] = [
+		{
+			link: '/',
+			icon: 'fa fa-home fa-lg'
+		},
+		{
+			link: '/store',
+			icon: 'fa fa-plus-circle fa-lg'
+		},
+		{
+			link: '/profile',
+			icon: 'fa fa-user fa-lg'
+		}
+	];
 
 	constructor(props: any) {
 		super(props);
 		this.state = {
 			widgets: []
 		};
+		this.createWidget = this.createWidget.bind(this);
+		this.onWidgetAdded = this.onWidgetAdded.bind(this);
 		this.deleteWidget = this.deleteWidget.bind(this);
 	}
 
@@ -41,6 +67,21 @@ export default class Dashboard extends React.Component<any, IState> {
 			.catch((error: Error) => {
 				logger.error(error.message);
 			});
+	}
+
+	public onWidgetAdded(type: any) {
+		addWidget(type.target.value)
+			.then((response) => {
+				if (response) {
+					const widgetData: IWidgetConfig = response.data;
+					this.createWidget(widgetData);
+					this.setState({ widgets: this.state.widgets.concat(widgetData) })
+				}
+			})
+			.catch(error => {
+				logger.error(error.message);
+			})
+
 	}
 
 	public createWidget(widgetConfig: IWidgetConfig) {
@@ -84,17 +125,34 @@ export default class Dashboard extends React.Component<any, IState> {
 	public render() {
 		return (
 			<div className="dash">
-				{
-					this.state.widgets &&
-					this.state.widgets.map((widgetConfig: IWidgetConfig) => {
-						return (
-							<div key={widgetConfig.id} className="widget">
-								{this.createWidget(widgetConfig)}
-							</div>
-						);
-					})
-				}
+				<Router>
+					<div className='flexRow'>
+						<Navbar navItems={this.navItems} />
 
+						<Switch>
+							<Route exact path="/">
+								<div className='widgetList'>
+									{
+										this.state.widgets &&
+										this.state.widgets.map((widgetConfig: IWidgetConfig) => {
+											return (
+												<div key={widgetConfig.id} className="widget">
+													{this.createWidget(widgetConfig)}
+												</div>
+											);
+										})
+									}
+								</div>
+							</Route>
+							<Route path="/store">
+								<Store onWidgetAdded={this.onWidgetAdded} />
+							</Route>
+							<Route path="/profile">
+
+							</Route>
+						</Switch>
+					</div>
+				</Router>
 			</div>
 
 		);
--- src/services/WidgetService.ts
+++ src/services/WidgetService.ts
@@ -1,10 +1,20 @@
 import axios from 'axios';
+import { WidgetTypes } from 'src/enums/WidgetsEnum';
+
+export const addWidget = (type: WidgetTypes) => {
+	return axios.post(`${process.env.REACT_APP_BACKEND_URL}/db/addWidget`, { "type": type },
+		{
+			headers: {
+				'Content-type': 'application/json'
+			}
+		});
+}
 
 export const updateWidget = (id: number, data: any) => {
-    return axios.post(`${process.env.REACT_APP_BACKEND_URL}/db/updateWidget`, { "id": id, "data": data },
-			{
-				headers: {
-					'Content-type': 'application/json'
-				}
-			})
+	return axios.post(`${process.env.REACT_APP_BACKEND_URL}/db/updateWidget`, { "id": id, "data": data },
+		{
+			headers: {
+				'Content-type': 'application/json'
+			}
+		});
 }
