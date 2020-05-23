8f13116afe3831c799c4a5a6676e3922d1d8041c
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2020-05-23 19:24:53 -0400

    credentials

--- src/server.js
+++ src/server.js
@@ -17,6 +17,7 @@ app.use(express.json())
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
