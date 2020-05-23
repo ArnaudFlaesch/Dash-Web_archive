aebbd19def1737887649d42b67d9e8fb650e9902
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2020-05-23 19:01:18 -0400

    cors any

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, '../build')));
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.HOST || '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
