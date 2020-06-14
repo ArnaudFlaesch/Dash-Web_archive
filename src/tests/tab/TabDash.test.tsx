7857613ecca2bc53f6060e0d917a86c4ebec5574
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2020-06-13 22:41:40 -0400

    TabDash test

--- src/tests/tab/TabDash.test.tsx
+++ src/tests/tab/TabDash.test.tsx
@@ -1,12 +1,34 @@
-import TabDash from "../../tab/TabDash";
-import * as ReactDOM from 'react-dom';
+import axios from 'axios';
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import TabDash from "../../tab/TabDash";
+import * as widgetDataSample from './widgetDataSample.json';
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
 
 describe("TabDash tests", () => {
     it('renders without crashing', () => {
-      const div = document.createElement('div');
-      ReactDOM.render(<TabDash />, div);
-      ReactDOM.unmountComponentAtNode(div);
+        const div = document.createElement('div');
+        ReactDOM.render(<TabDash />, div);
+        ReactDOM.unmountComponentAtNode(div);
     });
-  });
-  
+
+    it('should render a tab with 3 widgets', async () => {
+        const widgetServiceResponse = {
+            data: widgetDataSample
+        };
+
+        jest.spyOn(mockedAxios, "get").mockImplementation(() => {
+            return Promise.resolve(widgetServiceResponse)
+        });
+
+        const container = document.createElement("div");
+        document.body.appendChild(container);
+        await act(async () => {
+            ReactDOM.render(<TabDash tabId={1} />, container);
+        });
+        expect(container.getElementsByClassName('.widget').length).toEqual(5);
+    });
+});
