2c781fd720428c5eeaa399f6d49a37d1ed383764
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2020-06-13 22:19:57 -0400

    Navbar test

--- src/tests/navbar/Navbar.test.tsx
+++ src/tests/navbar/Navbar.test.tsx
@@ -1,6 +1,7 @@
 import Navbar  from "../../navbar/Navbar";
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
+import { shallow } from 'enzyme';
 
 describe("Navbar tests", () => {
     it('renders without crashing', () => {
@@ -8,5 +9,24 @@ describe("Navbar tests", () => {
       ReactDOM.render(<Navbar navItems={[]} />, div);
       ReactDOM.unmountComponentAtNode(div);
     });
+
+    it('should render 3 nav menu items', () => {
+        const navItems = [{
+			link: '/',
+			icon: 'fa fa-home fa-lg'
+		},
+		{
+			link: '/store',
+			icon: 'fa fa-plus-circle fa-lg'
+		},
+		{
+			link: '/profile',
+			icon: 'fa fa-user fa-lg'
+        }];
+        const component = shallow(
+            <Navbar navItems={navItems} />,
+          );
+        expect(component.find('dashNavbar').length).toEqual(3);
+      });
   });
   
