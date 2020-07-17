c5ff702a86e61c144c1c9e2a7dba0f7d8a5cf3b2
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2020-07-17 11:42:58 -0400

    Update Forecast.tsx

--- src/widgets/weather/forecast/Forecast.tsx
+++ src/widgets/weather/forecast/Forecast.tsx
@@ -12,7 +12,7 @@ const Forecast: React.FunctionComponent<IForecast> = props => {
                     <img src={`https://openweathermap.org/img/wn/${props.weather[0]?.icon}@2x.png`} className="smallImage" title={props.weather[0]?.description} alt={props.weather[0]?.description} />
                 </div>
                 <div>
-                    <div><i className="fa fa-thermometer-three-quarters fa-sm d-mr-10" style={{color: "lightred"}} />{props.main.temp_max}°</div>
+                    <div><i className="fa fa-thermometer-three-quarters fa-sm d-mr-10" style={{color: "crimson"}} />{props.main.temp_max}°</div>
                     <div><i className="fa fa-thermometer-quarter fa-sm d-mr-10" style={{color: "blue"}} />{props.main.temp_min}°</div>
                     <div><i className="fa fa-tint fa-sm d-mr-10" style={{color: "lightblue"}} />{props.main.humidity}%</div>
                 </div>
