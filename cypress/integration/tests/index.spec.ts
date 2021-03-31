15fc98f6e1f9ba9dd548dc21b5285d38738469f0
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-03-31 18:48:09 -0400

    hide record key

--- cypress/integration/tests/index.spec.ts
+++ cypress/integration/tests/index.spec.ts
@@ -9,7 +9,10 @@ context('Dash tests', () => {
     cy.title().should('equals', 'Dash');
     cy.get('#openAddWidgetModal').click();
     cy.get('.card-title').should('have.length', 4);
+    cy.intercept('POST', '/widget/addWidget').as('addWidget');
     cy.get('#RSS').click();
-    cy.get('.widget').should('have.length', 1);
+    cy.wait('@addWidget').then((xhr) => {
+      cy.get('.widget').should('have.length', 1);
+    });
   });
 });
