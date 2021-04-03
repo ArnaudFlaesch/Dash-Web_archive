150c7ac05f948f890d0bccda6b12a09de2771416
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-04-02 21:04:05 -0400

    test

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,4 +1,4 @@
-import { addDays } from "date-fns";
+import addDays from 'date-fns/addDays'
 
 export function getDayFromNow(numberOfDays: number): Date {
   return addDays(new Date(), numberOfDays);
