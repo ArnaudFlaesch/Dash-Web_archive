575a258bad5630abe6785b52e40a30e1fcc9c1ee
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-05-12 16:06:48 +0200

    fix eslint

--- src/widgets/twitter/TwitterWidget.tsx
+++ src/widgets/twitter/TwitterWidget.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import EmptyRSSWidget from '../rss/emptyWidget/EmptyRSSWidget';
 import Widget from '../Widget';
 import * as  crypto from "crypto";
-import * as  OAuth from "oauth-1.0a";
+import OAuth from "oauth-1.0a";
 import * as request from "request";
+import logger from "src/utils/LogUtils";
 
 interface IProps {
     id: number;
@@ -11,17 +12,17 @@ interface IProps {
     tabId: number;
 }
 
-export default function TwitterWidget(props: IProps) {
+export default function TwitterWidget(props: IProps): React.ReactElement {
 
     // Note: The token is optional for some requests
-    const token = {
-       key: '1339340779-hhYTPFrY5EFIieJC42WrXrs8Sg6eMH552Nqjff8',
-       secret: '1r3Kk519wzoqlaG0ii28yAc9nQ6aQYehMf2w9HBRHsc7d',
-    // key: "UwmrbQAAAAABG2eVAAABc",
-   //  secret: "qz06kWiQZjqjGUzhGERGmEbGRuOo4qXY"
+    const token: unknown = {
+        key: '1339340779-hhYTPFrY5EFIieJC42WrXrs8Sg6eMH552Nqjff8',
+        secret: '1r3Kk519wzoqlaG0ii28yAc9nQ6aQYehMf2w9HBRHsc7d',
+        // key: "UwmrbQAAAAABG2eVAAABc",
+        //  secret: "qz06kWiQZjqjGUzhGERGmEbGRuOo4qXY"
     }
 
-     // https://api.twitter.com/oauth/request_token
+    // https://api.twitter.com/oauth/request_token
 
     const request_data = {
         url: 'https://api.twitter.com/1.1/statuses/user_timeline.json?include_entities=true',
@@ -32,7 +33,7 @@ export default function TwitterWidget(props: IProps) {
     const oauth = new OAuth({
         consumer: { key: '6gpcEC2lrca54mxCZfKx2Rdbu', secret: 'jt9HmXhMrBl8xd7HZGRIpWz6SMir9fpu5NYSu4v6KJogRfQoH9' },
         signature_method: 'HMAC-SHA1',
-        hash_function(base_string, key) {
+        hash_function(base_string: string, key: string) {
             return crypto
                 .createHmac('sha1', key)
                 .update(base_string)
@@ -40,7 +41,7 @@ export default function TwitterWidget(props: IProps) {
         },
     })
 
-    request(
+    /* request(
         {
             url: request_data.url,
             method: request_data.method,
@@ -49,7 +50,7 @@ export default function TwitterWidget(props: IProps) {
         () => {
 
         }
-    )
+    ) */
     const widgetHeader =
         <div>Header</div>
 
@@ -61,8 +62,8 @@ export default function TwitterWidget(props: IProps) {
             config={{ "url": "url" }}
             header={widgetHeader}
             body={widgetBody}
-            editModeComponent={<EmptyRSSWidget url={""} onUrlSubmitted={() => { }} />}
-            refreshFunction={() => { }}
+            editModeComponent={<EmptyRSSWidget url={""} onUrlSubmitted={() => { return null }} />}
+            refreshFunction={() => { return null }}
             onDeleteButtonClicked={props.onDeleteButtonClicked} />
     </div>
 }
