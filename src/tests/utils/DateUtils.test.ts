76daf83d9c8a64b12198b27db648853bebfaaeed
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-05-29 21:10:05 +0200

    fix date test

--- src/tests/utils/DateUtils.test.ts
+++ src/tests/utils/DateUtils.test.ts
@@ -2,8 +2,8 @@ import { formatDateFromTimestamp } from 'src/utils/DateUtils';
 
 describe('DateUtils tests', () => {
   it('Should format date', () => {
-    expect(formatDateFromTimestamp(1592179200).toLocaleString()).toEqual(
-      '15/06/2020, 02:00:00'
+    expect(formatDateFromTimestamp(1592179200).toDateString()).toEqual(
+      'Mon Jun 15 2020'
     );
   });
 });
