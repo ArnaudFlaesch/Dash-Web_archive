7724467bd7d8b2a7ed466908fa390505cecb9f2c
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2021-06-12 16:19:39 +0200

    FT #28 refresh all widgets (#308)
    * FT #28 : Refresh all widgets
    
    * Add tests
    
    * Fix import order

--- src/Dash.tsx
+++ src/Dash.tsx
@@ -7,6 +7,7 @@ import {
   DroppableProvided,
   DropResult
 } from 'react-beautiful-dnd';
+import { emitCustomEvent } from 'react-custom-events';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, Nav, TabContent } from 'reactstrap';
 import './Dash.scss';
@@ -71,6 +72,10 @@ export default function Dash(): React.ReactElement {
     if (activeTab !== tab) {
       dispatch(toggleSelectedTab(tab));
     }
+  }
+
+  function refreshAllWidgets() {
+    emitCustomEvent('refreshAllWidgets');
   }
 
   function addNewTab() {
@@ -174,6 +179,13 @@ export default function Dash(): React.ReactElement {
             {activeTab && tabs.length > 0 && (
               <Nav vertical={true} navbar={true}>
                 <CreateWidgetModal onWidgetAdded={onWidgetAdded} />
+                <Button
+                  id="reloadAllWidgetsButton"
+                  className="dashNavbarLink"
+                  onClick={refreshAllWidgets}
+                >
+                  <i className="fa fa-refresh fa-lg" aria-hidden="true" />
+                </Button>
                 <Button
                   id="downloadConfigButton"
                   className="dashNavbarLink"
--- src/widgets/Widget.tsx
+++ src/widgets/Widget.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, useEffect, useState } from 'react';
+import { useCustomEventListener } from 'react-custom-events';
 import { ModeEnum } from '../enums/ModeEnum';
 import DeleteWidget from './utils/DeleteWidget';
 
@@ -15,6 +16,10 @@ interface IProps {
 
 export default function Widget(props: IProps): ReactElement {
   const [mode, setMode] = useState(ModeEnum.READ);
+
+  useCustomEventListener('refreshAllWidgets', () => {
+    props.refreshFunction();
+  });
 
   useEffect(() => {
     setMode(ModeEnum.READ);
