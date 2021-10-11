5671a2509fbf6ab8c90d082608fe4a65930babfc
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-10-11 21:53:08 +0200

    revert

--- src/reducers/tabReducer.ts
+++ src/reducers/tabReducer.ts
@@ -8,7 +8,7 @@ const initialState: ITabState = {
   activeTab: -1
 };
 
-export default function (action: { type: string; payload: { id: number } }, state = initialState): ITabState {
+export default function (state = initialState, action: { type: string; payload: { id: number } }): ITabState {
   switch (action.type) {
     case TOGGLE_TAB: {
       const { id } = action.payload;
