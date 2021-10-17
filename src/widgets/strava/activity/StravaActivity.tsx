1b08498512ff0ec49036eb876196c7ee0f208a2e
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-10-17 17:56:49 +0200

    fix strava display

--- src/widgets/strava/activity/StravaActivity.tsx
+++ src/widgets/strava/activity/StravaActivity.tsx
@@ -2,6 +2,12 @@ import { format } from 'date-fns';
 import { ReactElement } from 'react';
 import { IActivity } from '../IStrava';
 
+function convertDecimalTimeToTime(decimalTime: number): number {
+  const decimalPart = decimalTime % 1;
+  const convertedDecimalPart = (decimalPart / 10) * 6;
+  return decimalTime - decimalPart + convertedDecimalPart;
+}
+
 export default function StravaActivity(props: IActivity): ReactElement {
   return (
     <div>
@@ -10,9 +16,9 @@ export default function StravaActivity(props: IActivity): ReactElement {
           {format(new Date(props.start_date_local), 'dd MMM')} {props.name}
         </a>
       </div>
-      <div>Distance : {Math.round(props.distance * 1000) / 10000} kms</div>
-      <div>Durée : {props.moving_time / 60} mins</div>
-      <div>Denivelé : {props.total_elevation_gain}</div>
+      <div>Distance : {Math.round(props.distance) / 1000} kms</div>
+      <div>Durée : {convertDecimalTimeToTime(props.moving_time / 60)} mins</div>
+      <div>Denivelé : {props.total_elevation_gain} mètres</div>
       <div>Trophées : {props.achievement_count}</div>
       <div>Vitesse moyenne : {props.average_speed}</div>
       <div>Vitesse max : {props.max_speed}</div>
