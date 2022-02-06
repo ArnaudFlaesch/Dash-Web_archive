ec1f5e9f95a2a29f341c1bf85d23760ee92cea29
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-02-06 18:59:31 +0100

    linear

--- src/widgets/weather/WeatherWidget.tsx
+++ src/widgets/weather/WeatherWidget.tsx
@@ -17,7 +17,7 @@ import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import Brightness3Icon from '@mui/icons-material/Brightness3';
 import ScheduleIcon from '@mui/icons-material/Schedule';
 import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
-import { CategoryScale } from 'chart.js';
+import { CategoryScale, LinearScale } from 'chart.js';
 import { Chart as ChartJS } from 'chart.js';
 interface IProps extends IBaseWidgetConfig {
   city?: string;
@@ -41,7 +41,7 @@ export default function WeatherWidget(props: IProps): React.ReactElement {
   const [city, setCity] = useState<ICity>();
   const [forecastMode, setForecastMode] = useState<ForecastMode>(ForecastMode.TODAY);
 
-  ChartJS.register(CategoryScale);
+  ChartJS.register(CategoryScale, LinearScale);
 
   function fetchDataFromWeatherApi() {
     if (cityToQuery) {
